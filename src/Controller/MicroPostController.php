7c1ae7f564bbafa0d71cdb0540896b695efb265c
RubenMT1999 <rmatiastiscar@safareyes.es>
2022-12-14 18:54:27 +0100

    primer metodo api

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,18 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(): JsonResponse
+
+    private $microPostRepository;
+
+    public function __construct(MicroPostRepository $microPostRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MicroPostController.php',
-        ]);
+        $this->microPostRepository = $microPostRepository;
     }
+
+
+    #[Route('/micro/post', name: 'app_micro_post')]
+    public function getAll(): JsonResponse
+    {
+        /* $microPost = new MicroPost();
+        $microPost->setTitle('It comes from controller');
+        $microPost->setText('Hi!');
+        $microPost->setCreated(new DateTime()); */
+
+        /* $microPost = $posts->find(2);
+        $microPost->setTitle('Welcome in general');
+
+        $posts->save($microPost,true); */
+
+        //dd($posts->findAll());
+
+        $micros = $this->microPostRepository->findAll();
+        $data = [];
+
+        foreach($micros as $micro){
+            $data[] = [
+                'id' => $micro->getId(),
+                'title' => $micro->getTitle(),
+                'text' => $micro->getText(),
+                'created' => $micro->getCreated(),
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+
+
+
 }
