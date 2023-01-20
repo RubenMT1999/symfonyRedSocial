ec4a17509a2092a64bc34846c34ab52d7b4fdcc5
Pedrofdez97 <pfernandezsalas@safareyes.es>
2023-01-20 17:15:44 +0100

    BBDD

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserProfileRepository;
 use App\Repository\UserRepository;
 use App\utils\Utils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,4 +35,29 @@ class UserController extends AbstractController
         $listJson = $utilidades -> toJson($listUsuario);
         return new JsonResponse($listJson, 200,[], true);
     }
+
+    #[Route('/user/list/name',  name: 'app_user_list_name' ,methods:['POST'])]
+    public function list_name(UserProfileRepository $userProfileRepository,UserRepository $userRepository,Utils $utilidades, Request $request): JsonResponse
+    {
+        // $nombre = $request -> request ->get('name');
+        // $busqueda = array('name'=> $nombre);
+        $data = json_decode($request->getContent(),true);
+        $userProfile = $userRepository->findOneBy(['email' => $data]);
+        $listProfile = $userProfileRepository->findOneBy(['user' => $userProfile]);
+        $data2[] = [
+            'name' => $listProfile->getName(),
+            'bio' => $listProfile->getBio(),
+            'website_url' => $listProfile->getWebsiteUrl(),
+            'username' => $listProfile->getTwitterUsername(),
+            'company' => $listProfile->getCompany(),
+            'location' => $listProfile->getLocation(),
+            'email' => $listProfile->getUser()->getEmail(),
+            'date_of_birth' => $listProfile->getDateOfBirth()
+
+            ];
+
+        $listJson = $utilidades -> toJson($data2);
+        return new JsonResponse($listJson, 200,[], true);
+    }
+
 }
