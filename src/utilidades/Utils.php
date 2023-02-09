5b3be67933057e9abcb92796830156449f402317
corsev <acornejolopez@safareyes.es>
2023-02-09 19:06:35 +0100

    añadido usuario token

--- src/utilidades/Utils.php
+++ src/utilidades/Utils.php
@@ -2,6 +2,8 @@
 
 namespace App\utilidades;
 
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -21,6 +23,28 @@ class Utils
         $json = $serializer->serialize($data, 'json');
 
         return $json;
+    }
+
+    public function obtenerUsuarioToken(Request $request): User
+    {
+
+
+        // Obtener el token JWT del encabezado de la solicitud
+
+        $token = $request->headers->get('Authorization');
+        if ($token) {
+            // Decodificar el token JWT y obtener el contenido
+            $data = $this->jwtEncoder->decode($token);
+
+            $usuario = new User();
+
+            $usuario->setEmail($data['username']);
+
+
+        }
+
+
+        return $usuario;
     }
 
 }
